ecd95d0c6d0f6c9910c3227f8868611dee320146
master <spikerus93@rambler.ru>
2023-07-08 11:26:13 +0300

    Решение задач на 9-ом Семинаре-Задачи 63,65,69

---
+++ task67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите любое число от 100 до 1000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumbers(int number)
+{
+    if (number <= 0) return 0;
+    return SumOfNumbers(number / 10) + (number % 10);
+}
+
+SumOfNumbers(number);
+Console.Write(SumOfNumbers(number));
---
+++ task69/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число А:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int Stepen(int osnova, int mnozhitel)
+{
+    if (mnozhitel == 0) return 1;
+    return Stepen(osnova, mnozhitel-1) * osnova;
+}
+
+Console.WriteLine(Stepen(A, B));
